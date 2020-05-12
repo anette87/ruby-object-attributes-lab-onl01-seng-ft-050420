29df23f61afa6016f4dbde9c9f55238742d1114e
Anette Rivera <anettem.rivera@gmail.com>
2020-05-12 18:16:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,8 +7,33 @@ class Person
   def name
     @name
   end 
-
+  
+  def job= (person_job)
+    @job = person_job
+  end 
+  
+  def job
+    @job
+  end 
+    
+    
+    
+    
+    
 beyonce = Person.new
 beyonce.name = "Beyonce"
+beyonce.job = "Singer"
   
 end 
+
+
+
+
+
+
+
+  # with jobs
+  #   #job=
+  #     writes the job of the person to an instance variable @job (FAILED - 1)
+  #   #job
+  #     reads the job of the person from an instance variable @job (FAILED - 2)
